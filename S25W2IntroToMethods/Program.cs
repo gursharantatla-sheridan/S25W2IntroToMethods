ce4091fbfa30f8d0ececa5e6d01c894f049d9bff
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-14 12:32:43 -0400

    circle

--- S25W2IntroToMethods/Program.cs
+++ S25W2IntroToMethods/Program.cs
@@ -6,22 +6,32 @@
         {
             int num = 5;
             string str = "hello";
+            Circle cir = new Circle();
+            cir.radius = 5;
 
             Console.WriteLine("Before changes:\n");
             Console.WriteLine("num = " + num);
             Console.WriteLine("str = " + str);
+            Console.WriteLine("cir.radius = " + cir.radius);
 
-            ChangeValue(num, str);
+            ChangeValue(num, str, cir);
 
             Console.WriteLine("\nAfter changes:\n");
             Console.WriteLine("num = " + num);
             Console.WriteLine("str = " + str);
+            Console.WriteLine("cir.radius = " + cir.radius);
         }
 
-        static void ChangeValue(int n, string s)
+        static void ChangeValue(int n, string s, Circle c)
         {
             n = 100;
             s = "world";
+            c.radius = 100;
         }
+    }
+
+    class Circle
+    {
+        public double radius;
     }
 }
