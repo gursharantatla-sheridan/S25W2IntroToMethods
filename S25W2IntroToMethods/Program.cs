29c505e248679dd29dddf3d5dca9352ca02593bd
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-14 12:40:38 -0400

    arr

--- S25W2IntroToMethods/Program.cs
+++ S25W2IntroToMethods/Program.cs
@@ -8,25 +8,29 @@
             string str = "hello";
             Circle cir = new Circle();
             cir.radius = 5;
+            int[] arr = { 1, 2, 3, 4, 5 };
 
             Console.WriteLine("Before changes:\n");
             Console.WriteLine("num = " + num);
             Console.WriteLine("str = " + str);
             Console.WriteLine("cir.radius = " + cir.radius);
+            Console.WriteLine("arr[0] = " + arr[0]);
 
-            ChangeValue(num, str, cir);
+            ChangeValue(num, str, cir, arr);
 
             Console.WriteLine("\nAfter changes:\n");
             Console.WriteLine("num = " + num);
             Console.WriteLine("str = " + str);
             Console.WriteLine("cir.radius = " + cir.radius);
+            Console.WriteLine("arr[0] = " + arr[0]);
         }
 
-        static void ChangeValue(int n, string s, Circle c)
+        static void ChangeValue(int n, string s, Circle c, int[] a)
         {
             n = 100;
             s = "world";
             c.radius = 100;
+            a[0] = 100;
         }
     }
 
